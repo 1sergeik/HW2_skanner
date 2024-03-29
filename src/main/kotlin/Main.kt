a21053275df1ffc5364e183cd4512d749c712d4a
1sergeik <1sergeik@gmail.com>
2024-03-29 11:13:18 +0300

    task HW2 completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,12 @@ fun main() {
     println("введите первое число")
     val a = input.nextInt()  //ввод первого числа
     println("введите второе число")
-    val b = input.nextInt()
+    val b = input.nextInt()  //ввод второго числа
     println("введите третье число")
-    val c = input.nextInt()
+    val c = input.nextInt()  //ввод третьего числа
     summa(a,b,c )
 }
 fun summa(a:Int, b:Int, c:Int) {
     println("сумма трех чисел составляет")
-    println(a+b+c)
+    println(a+b+c)   // суммирование и печать в консоль
 }
